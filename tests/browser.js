f671d7fdd3b69cd50ae7527a3f7066e1536d511b
Daniel Wood <daniel.wood4@extendhealth.com>
2015-12-25 22:06:59 -0700

    implemented babel-tape-runner and colored-tape testing

---
+++ tests/browser.js
@@ -0,0 +1,11 @@
+import browser from '../src/browser';
+import scan from '../src/scan';
+import test from 'colored-tape';
+
+test('scan test', function (assert) {
+  // assert.plan(1);
+  assert.equal(1, 1, '1 should equal 1');
+  assert.ok(browser, 'browser should be OK');
+  assert.ok(scan, 'scan should be OK');
+  assert.end();
+});
