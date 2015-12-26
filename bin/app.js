159d7e2f3ba8b69c2fac0890dafcf26d8815dc25
Daniel Wood <daniel.wood4@extendhealth.com>
2015-12-25 19:01:22 -0700

    finished update to babel-6, cleaned up npm publishing

--- bin/app.js
+++ bin/app.js
@@ -1,10 +1,6 @@
 #!/usr/bin/env node
 'use strict';
 
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj['default'] = obj; return newObj; } }
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
-
 var _externalip = require('externalip');
 
 var _externalip2 = _interopRequireDefault(_externalip);
@@ -37,29 +33,35 @@ var _contentDisposition = require('content-disposition');
 
 var _contentDisposition2 = _interopRequireDefault(_contentDisposition);
 
-//const MAXINT = Number.MAX_SAFE_INTEGER;
-var argOpts = {};
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
-var argv = (0, _minimist2['default'])(process.argv, argOpts);
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+//const MAXINT = Number.MAX_SAFE_INTEGER;
+var argOpts = {
+	//    '-p':'port';
+};
+
+var argv = (0, _minimist2.default)(process.argv, argOpts);
 var port = argv.port || 3000;
 var dir = argv.dir || '.';
 
-(0, _externalip2['default'])(function (err, ip) {
+(0, _externalip2.default)(function (err, ip) {
 	var local = 'http://' + ip + ':' + port;
 	tiny.shorten(local, function (shortUrl, err) {
-		_copyPaste2['default'].copy(shortUrl);
+		_copyPaste2.default.copy(shortUrl);
 		console.log('.\n.\n.\nAcquired TinyUrl: ' + shortUrl + '\n' + '***Copied URL to clipboard***\n' + 'SocketShare is running on port ' + port +
 		//`\nExternal IP: ${local}`+
 		'!\nMake sure you open this port on your router!!!' + '\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^' + '\n---Press CTRL + C to stop the server---');
 	});
 });
-var tree = (0, _scan2['default'])(dir, 'files');
-var app = (0, _express2['default'])();
-app.use('/', _express2['default']['static'](_path2['default'].join(__dirname, 'frontend')));
-app.use('/files', _express2['default']['static'](process.cwd(), {
+var tree = (0, _scan2.default)(dir, 'files');
+var app = (0, _express2.default)();
+app.use('/', _express2.default.static(_path2.default.join(__dirname, '../public')));
+app.use('/files', _express2.default.static(process.cwd(), {
 	index: false,
 	setHeaders: function setHeaders(res, path) {
-		res.setHeader('Content-Disposition', (0, _contentDisposition2['default'])(path));
+		res.setHeader('Content-Disposition', (0, _contentDisposition2.default)(path));
 	}
 }));
 
@@ -67,5 +69,3 @@ app.get('/scan', function (req, res) {
 	res.send(tree);
 });
 app.listen(port);
-
-//    '-p':'port';
